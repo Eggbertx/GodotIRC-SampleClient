c74ce32dd3ba77c52b6fe05324d646f89c1761e9
Eggbertx <eggbertx@users.noreply.github.com>
2024-04-13 16:04:27 -0700

    Start message processing

--- src/main.gd
+++ src/main.gd
@@ -1,5 +1,6 @@
 extends Node
 
+@onready var irc_mgr:IRCClientManager  = $IRCClientManager
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,13 +11,12 @@ func _ready():
 func _process(delta):
 	pass
 
-
 func _on_ui_server_item_selected(id: int):
 	match id:
 		Menus.SERVER_EXIT:
-			print("Exiting")
-			get_tree().quit()
+			irc_mgr.disconnect_clients()
 		_:
 			print("unhandled server menu id %d" % id)
 
-
+func _on_irc_client_manager_unhandled_message_received(client, msg):
+	print("%s <- %s" % [client.host, msg])
